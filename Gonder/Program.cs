0f5f8d83bc78eecdbff5c99db8813ccb3ee5e764
ssenatekin <sado-tekin@hotmail.com>
2023-10-19 15:57:10 +0300

    Add project files.

---
+++ Gonder/Program.cs
@@ -0,0 +1,34 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Gonder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                MesajGonder();
+            }
+        }
+
+        
+        public static void MesajGonder()
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqps://xwkhbjsj:hJB6iqSOkuL9iRqDGbm7YyCR3LeHwG7h@octopus.rmq3.cloudamqp.com/xwkhbjsj");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("mesaj-kuyruk", true, false, false);
+            var mesaj = "Sena Tekin Mesaj.";
+            var body = Encoding.UTF8.GetBytes(mesaj);
+            channel.BasicPublish(String.Empty, "mesaj-kuyruk", null, body);
+
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MesajAl/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace MesajAl
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.Uri=new Uri("amqps://xwkhbjsj:hJB6iqSOkuL9iRqDGbm7YyCR3LeHwG7h@octopus.rmq3.cloudamqp.com/xwkhbjsj");
+            using var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+            channel.QueueDeclare("mesaj-kuyruk", true, false, false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            channel.BasicConsume("mesaj-kuyruk",true,consumer);
+
+            consumer.Received += Consumer_Received;
+            Console.ReadLine();
+
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            Console.WriteLine("Gelen Mesaj:" + Encoding.UTF8.GetString(e.Body.ToArray()));
+           
+        }
+    }
+}
